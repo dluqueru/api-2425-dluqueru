58789fff694b53072b77b5f8374c81f912108b8d
dluqueru <dluqueru@gmail.com>
2025-06-09 23:42:42 +0200

    incluyo url del frontend desplegado para evitar error de CORS

--- src/main/java/com/checkpointBlog/security/SecurityConfig.java
+++ src/main/java/com/checkpointBlog/security/SecurityConfig.java
@@ -77,8 +77,8 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
-        		"http://localhost:4200",
-        		"https://api-2425-dluqueru-javaclean.onrender.com"));
+        		"https://checkpoint-blog-psi.vercel.app",
+        		"http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Access-Control-Request-Headers"));
         config.setAllowCredentials(true);
