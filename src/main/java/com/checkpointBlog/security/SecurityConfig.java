7c2b13d26b1c12ff1595c22b12b61f4a4cd4ed81
dluqueru <dluqueru@gmail.com>
2025-06-12 19:03:32 +0200

    reordeno requestMatchers para solucionar bug que no permitía a usuarios READER reportar artículos

--- src/main/java/com/checkpointBlog/security/SecurityConfig.java
+++ src/main/java/com/checkpointBlog/security/SecurityConfig.java
@@ -70,10 +70,10 @@ public class SecurityConfig {
         	        "/category/**",
         	        "/swagger-ui/**",
         	        "/v3/api-docs/**").permitAll()
+        	    .requestMatchers("/article/**", "/likes/**", "/article/*/report", "/article/drafts").authenticated()
                 .requestMatchers(HttpMethod.POST, "/article", "/api/images/upload/**").hasAnyAuthority("ADMIN", "EDITOR")
                 .requestMatchers(HttpMethod.PUT, "/article/**").hasAnyAuthority("ADMIN", "EDITOR")
                 .requestMatchers(HttpMethod.DELETE, "/article/**", "/api/images/**").hasAnyAuthority("ADMIN", "EDITOR")
-                .requestMatchers("/article/**", "/likes/**", "/article/*/report", "/article/drafts").authenticated()
                 .requestMatchers("/article/*/unreport", "/article/reported").hasRole("ADMIN")
                 .anyRequest().denyAll()
             )
